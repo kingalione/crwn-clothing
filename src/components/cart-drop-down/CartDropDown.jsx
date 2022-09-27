c1e54c01ba6da6a67dad3d06a3b49ff7d3fa0cf8
Ali <akrdg1988@gmail.com>
2022-09-27 22:53:11 +0200

    add cartitems functionality
    - add cartitems functionality

--- src/components/cart-drop-down/CartDropDown.jsx
+++ src/components/cart-drop-down/CartDropDown.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import Button from '../button/Button';
+import React, { useContext } from "react";
+import Button from "../button/Button";
 
-import './CartDropDown.styles.scss';
+import { CartContext } from "../../contexts/CartContext";
+
+import CartItem from "../cart-item/CartItem";
+
+import "./CartDropDown.styles.scss";
 
 const CartDropDown = () => {
-  return (
-    <div className='cart-dropdown-container'>
-        <div className='cart-items'>
-            
-        </div>
-        <Button>To checkout</Button>
-    </div>
-  )
-}
+  const { cartItems } = useContext(CartContext);
 
-export default CartDropDown
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+      </div>
+      <Button>To checkout</Button>
+    </div>
+  );
+};
+
+export default CartDropDown;
