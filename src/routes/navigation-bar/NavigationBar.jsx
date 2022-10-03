6f0b6269d530aceae028c3a0243f9b2ed5aaff33
Ali <akrdg1988@gmail.com>
2022-10-03 14:52:24 +0200

    install styled components
    - implement styled components to NavigationBar

--- src/routes/navigation-bar/NavigationBar.jsx
+++ src/routes/navigation-bar/NavigationBar.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { ReactComponent as CrnwLogo } from "../../assets/crown.svg";
 
 import CartIcon from "../../components/cart-icon/CartIcon";
@@ -9,7 +9,12 @@ import { UserContext } from "../../contexts/UserContext";
 import { CartContext } from "../../contexts/CartContext";
 import { signOutUser } from "../../utils/firebase/Firebase";
 
-import "./NavigationBar.styles.scss";
+import {
+  NavigationContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink,
+} from "./NavigationBar.styles.jsx";
 
 const NavigationBar = () => {
   const { currentUser } = useContext(UserContext);
@@ -17,28 +22,25 @@ const NavigationBar = () => {
 
   return (
     <Fragment>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
+      <NavigationContainer>
+        <LogoContainer to="/">
           <CrnwLogo className="logo" />
-        </Link>
-        <div className="nav-links-container">
-          <Link to="/shop" className="nav-link">
-            Shop
-          </Link>
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to="/shop">Shop</NavLink>
+
           {currentUser ? (
-            <span className="nav-link" onClick={signOutUser}>
+            <NavLink as="span" onClick={signOutUser}>
               Sign Out
-            </span>
+            </NavLink>
           ) : (
-            <Link to="/auth" className="nav-link">
-              Sign In
-            </Link>
+            <NavLink to="/auth">Sign In</NavLink>
           )}
 
           <CartIcon />
-        </div>
+        </NavLinks>
         {isCartOpen && <CartDropDown />}
-      </div>
+      </NavigationContainer>
       <Outlet />
     </Fragment>
   );
