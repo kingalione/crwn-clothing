522c5b3045b1ffe65550ae8dbb29bd9dbeb03dd9
Ali <akrdg1988@gmail.com>
2022-10-03 15:39:03 +0200

    implement styled components to Button component
    - implement styled components to Button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+} from './Button.styles';
 
-import './Button.styles.scss';
-
-const BUTTON_TYPE_CLASSES = {
-  google: "google-sign-in",
-  inverted: "inverted",
+export const BUTTON_TYPE_CLASSES = {
+  base: 'base',
+  google: 'google-sign-in',
+  inverted: 'inverted',
 };
 
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
+  ({
+    [BUTTON_TYPE_CLASSES.base]: BaseButton,
+    [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+    [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+  }[buttonType]);
+
 const Button = ({ children, buttonType, ...otherProps }) => {
-  return (
-    <button
-      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
+  const CustomBottom = getButton(buttonType);
+  return <CustomBottom {...otherProps}>{children}</CustomBottom>;
 };
 
 export default Button;
