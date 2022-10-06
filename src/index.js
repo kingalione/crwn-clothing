84b39f9413cb788f5470d4e5e3df3c09b41815a6
Ali <akrdg1988@gmail.com>
2022-10-07 00:27:48 +0200

    install persistor library
    - implement persistor to store so the cart is saved in local storage

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
-import { store } from "./store/Store";
+import { store, persistor } from "./store/Store";
 
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate persistor={persistor} loading={null}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
 
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,4 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import logger from "redux-logger";
 
 import { rootReducer } from "./RootReducer";
@@ -20,7 +23,17 @@ const loggerMiddleware = (store) => (next) => (action) => {
 // root-reducer
 const middleWares = [logger];
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: middleWares,
 });
+
+export const persistor = persistStore(store);
